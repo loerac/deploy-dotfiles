70a6a5799515c132bcccb832afdffc2ed2c9ae2a
Brett Holman <bpholman5@gmail.com>
2018-02-06 12:38:03 -0600

    adding support for centOS

--- dependency_install.sh
+++ dependency_install.sh
@@ -1,8 +1,16 @@
+
+# get os-type to support debian AND centos
+if [ "$(. /etc/os-release; echo $NAME)" = "Ubuntu" ]; then
+	INSTALL='apt-get install'
+else
+	INSTALL='yum install' 
+fi
+
 # install vim
-sudo apt-get install vim -y
+eval "sudo $INSTALL vim -y"
 
 # install tmux
-sudo apt-get install tmux -y
+eval "sudo $INSTALL tmux -y"
 
 # Example for installing Vundle, the Vim dependency manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
