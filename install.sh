b66f35ced557a99c1e6a5e166dd0983d3adef97b
Brett Holman <holmanb0214@my.uwstout.edu>
2018-03-18 16:28:39 -0500

    improved printed messages

--- install.sh
+++ install.sh
@@ -2,43 +2,48 @@
 
 
 # Copy the dotfiles
-echo "copying dotfiles...   "
+echo -n "copying dotfiles...   " 
 cp .vimrc ~/
 cp .tmux.conf ~/
 cat .bashrc >> ~/.bashrc
 cp .Xdefaults ~/
 cp .printActiveIP.sh ~/
 
-# i3 setup
-mkdir ~/.config/i3/config -p
-mkdir ~/Downloads -p
-echo "exec --nostartup-id feh --bg-scale ~/Downloads/wall.jpg" >> ~/.config/i3/config
 
-echo "[dotfiles copied]"
+# i3 setup
+mkdir ~/.config/i3/ -p 
+cp wall.jpg ~/.config/i3
+echo "exec --nostartup-id feh --bg-scale ~/.config/i3/wall.jpg" >> ~/.config/i3/config
+
+echo "Success! [dotfiles copied]"
+echo ""
 
 # Install any required dependencies
-echo "install applications from dependencies.sh? [y/n]"
+echo -n "install applications from dependencies.sh? [y/n] "
 read install
 install=$(echo "$install" | awk '{print tolower($0)}')
 
 if [ "y" == "$install" ]; then
-	echo "installing dependencies...   "
+	echo -n "installing dependencies...   " 
 	/bin/bash dependencies.sh 
-	echo "[dependencies installed]"
+	echo "Success! [dependencies installed]"
+    echo ""
 fi
 
 # Setup git config
-echo "Setup git settings? [y/n]"
+echo -n "Setup git settings? [y/n] "
 read git_conf
 git_conf=$(echo "$git_conf" | awk '{print tolower($0)}')
 
 # Git config
 if [ "y" == "$git_conf" ]; then
-	echo "Enter user.name: "
+	echo -n "Enter user.name: " 
 	read name
-	echo "Enter user.email: "
+    echo ""
+	echo -n "Enter user.email: " 
 	read email
+    echo ""
 	git config --global user.name "$name"
 	git config --global user.email "$email"
-    echo "[git settings setup]"
+    echo "Success! [git settings setup]"
 fi
